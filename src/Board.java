d26ef6dc400577fb2022cce5150bd81e958ee922
KT <thun.kristian@gmail.com>
2023-10-23 14:20:17 +0200

    Half of board Working

--- src/Board.java
+++ src/Board.java
@@ -14,4 +14,57 @@ public class Board {
         return board;
     }
 
+    public void fillBoard(){
+        for (int i = 0; i < board.length; i++){
+            board[i]= new Tile();
+        }
+    }
+
+    public String toString(){
+      StringBuilder builder = new StringBuilder();
+      builder.append("   ");
+      for (int i = 0; i < width; i++){
+          builder.append("  ");
+          builder.append(i+1);
+          builder.append(" ");
+      }
+      builder.append("\n");
+      builder.append(drawLine(width));
+      for (int i = 0; i < board.length; i++){
+        if( i % width == 0){
+            if(i != 0){
+                builder.append("\n");
+            }
+
+            char letterForColumn = (char) ('A' + i/width);
+            String changeRowAndAddLetter = "\n " +letterForColumn + " ";
+            builder.append(changeRowAndAddLetter);
+        }
+
+        builder.append(board[i].isOpen() ? board[i].getBombsAround() : "| X ");
+        if (i % width == width -1 ){
+            builder.append("|");
+        }
+      }
+      builder.append("\n");
+      builder.append(drawLine(width));
+      return builder.toString();
+    }
+
+    public String drawLine(int width){
+      StringBuilder builder = new StringBuilder();
+      for (int i = 0; i < width; i++){
+          if (i == 0){
+              builder.append("   +----");
+          } else if (i == width-1) {
+              builder.append("---+");
+          }else {
+              builder.append("----");
+          }
+      }
+
+      return builder.toString();
+
+    }
+
 }
