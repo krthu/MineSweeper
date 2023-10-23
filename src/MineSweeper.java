d14695ae05c54e7d47a83fb2ff17d7168c8734b2
KT <thun.kristian@gmail.com>
2023-10-23 20:21:32 +0200

    getIndexFromCoordinate is working but annoying bug on negative input

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -1,3 +1,23 @@
 public class MineSweeper {
+    private Board board;
+
+    public MineSweeper(){
+
+    }
+
+    public int getIndexFromCoordinate(int width, int height, String coordinate){
+        int row = coordinate.toUpperCase().charAt(0) - 'A';
+        try{
+            int column = Integer.parseInt((coordinate.substring(1)))-1;
+            if (column >= width || row >= height || column < 0 || height < 0){
+                return -1;
+            }
+            return (row * width + column);
+
+        }catch (Exception e){
+            return -1;
+        }
+    }
+
 
 }
