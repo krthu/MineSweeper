0a136b7280756c10d42e56598fb368982ec2a8ff
irfan <isarac89@gmail.com>
2023-10-26 19:37:33 +0200

    rules

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         MineSweeper game = new MineSweeper();
         String meny = "";
-        while (!meny.equals("4")){
+        while (!meny.equals("5")){
             System.out.println("""
                     1.play Mine Sweeper
                     2.Settings
                     3.Show Stats
-                    4.Quit
+                    4.Rules
+                    5.Quit
                     """);
             meny = sc.nextLine();
             if (meny.equals("1")){
@@ -21,8 +22,12 @@ public class Main {
             }
             if (meny.equals("3")){
                 game.stats();
+
             }
             if (meny.equals("4")){
+                game.rules();
+            }
+            if (meny.equals("5")){
                 System.out.println("Game Ended");
             }
         }
